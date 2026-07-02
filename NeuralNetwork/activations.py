982da7876a6b2c28b4a1ef2b8d9990e10e223a18
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-07-02 19:35:10 +0530

    Implemented ReLU, Tanh and Sigmoid Activation functions

--- NeuralNetwork/activations.py
+++ NeuralNetwork/activations.py
@@ -1,22 +1,28 @@
+import numpy as np
 class ReLU:
     def __init__(self):
         pass
 
     def forward(self, x):
-        pass
+        self.x = x
+        return np.maximum(0, x)
 
     def backward(self, out_grad):
-        pass
+        return (np.maximum(0, self.x) > 0) * out_grad
 
 class Tanh:
     def __init__(self):
         pass
 
     def forward(self, x):
-        pass
+        self.x = x
+        neu = np.exp(x) - np.exp(-x)
+        deo = np.exp(x) + np.exp(-x)
+        self.y = neu / deo
+        return self.y
 
     def backward(self, out_grad):
-        pass
+        return (1 - (self.y ** 2)) * out_grad
 
 class Softmax:
     def __init__(self):
@@ -33,7 +39,9 @@ class Sigmoid:
         pass
 
     def forward(self, x):
-        pass
+        self.x = x
+        self.out = 1 / (1 + np.exp(-self.x))
+        return self.out
 
     def backward(self, out_grad):
-        pass
+        return self.out * (1 - self.out) * out_grad
